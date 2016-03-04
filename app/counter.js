db59e8e667a945917231558e42f988b1eef7ac12
Michal Ostruszka <michal.ostruszka@gmail.com>
2016-03-04 23:53:42 +0100

    Clean up - remove old index in favor of generated one, unify formatting

--- app/counter.js
+++ app/counter.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
-
 export default class Counter extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { count: 0 };
+    this.state = {count: 0};
   }
 
   componentWillMount() {
-  	setInterval(() => {
-  		this.setState({ count: this.state.count + 1 });
-  	}, 1000);
+    setInterval(() => {
+      this.setState({count: this.state.count + 1});
+    }, 1000);
   }
 
   render() {
     return (
-        <h2>The count is counting: { this.state.count }</h2>
-    );
+      <h2>The count is counting: {this.state.count}</h2>
+    )
   }
+
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,61 +4,65 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 var config = {
-	
-	entry: [
-		path.resolve(__dirname, "app/app.js")
-	],
 
-	output: {
-		path: __dirname + '/build',
-		publicPath: '/',
-		filename: './bundle.js'
-	},
+  entry: [
+    path.resolve(__dirname, "app/app.js")
+  ],
 
-	devtool: "cheap-eval-source-map",
+  output: {
+    path: __dirname + '/build',
+    publicPath: '/',
+    filename: './bundle.js'
+  },
 
-	module: {
-		loaders: [
-			{
-			  test: /\.css$/, loader: "style!css",
-			  exclude: /node_modules/
-			},
-			{ 
-			  test   : /.js$/,
-			  loader : 'babel-loader' ,
-			  exclude: /node_modules/
-			}
-		]
-	},
+  devtool: "eval-source-map",
 
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NoErrorsPlugin()
-	]
+  module: {
+    loaders: [
+      {
+        test: /\.css$/, loader: "style!css",
+        exclude: /node_modules/
+      },
+      {
+        test: /.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.tpl.html',
+      inject: true
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
 
 };
 
-if(process.env.NODE_ENV === 'production') {
-	config.devtool = '#source-map';
-	config.plugins = [
-		new CleanWebpackPlugin(['build']),
-		new webpack.optimize.DedupePlugin(),
-		new webpack.optimize.UglifyJsPlugin({
-			minimize: true,
-            mangle: true,
-            compress: {
-                warnings: false
-            },
-            output: {
-                comments: false
-            }
-		}),
-		new webpack.optimize.OccurenceOrderPlugin(),
-		new HtmlWebpackPlugin({
-			template: 'app/index.tpl.html',
-			inject: true
-		})
-	]
+if (process.env.NODE_ENV === 'production') {
+  config.devtool = '#source-map';
+  config.plugins = [
+    new CleanWebpackPlugin(['build']),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      mangle: true,
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'app/index.tpl.html',
+      inject: true
+    })
+  ]
 }
 
 module.exports = config;
